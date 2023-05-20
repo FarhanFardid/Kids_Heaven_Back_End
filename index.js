ec5f07e77d772573d1521a0b7b51f1b61dec1d10
FarhanFardid <farhanfardid009@gmail.com>
2023-05-20 18:35:00 +0600

    delete method implemented

--- index.js
+++ index.js
@@ -57,7 +57,13 @@ async function run() {
     const result = await toyCollection.findOne(query)
     res.send(result)
  })
-
+ 
+ app.delete('/toys/:id', async(req,res)=>{
+  const id =req.params.id;
+  const query = {_id : new ObjectId(id)}
+  const result = await toyCollection.deleteOne(query);
+  res.send(result)
+ })
 
 
       app.post('/toys', async (req,res)=> {
