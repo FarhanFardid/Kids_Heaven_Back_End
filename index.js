139b7fad328e96b1ca7b3c8637414a0d9b03cb5a
FarhanFardid <farhanfardid009@gmail.com>
2023-05-20 15:53:04 +0600

    get method implemented for stored toys

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT||5000;
@@ -15,7 +16,7 @@ app.get('/',(req,res)=>{
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.joz6qi9.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +32,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toyCollection = client.db("ToyDB").collection("toys");
+
+ app.get('/toys', async(req,res)=>{
+    const cursor = toyCollection.find()
+    const result = await cursor.toArray()
+    res.send(result);
+ })
+ app.get('/toys/:id', async (req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await toyCollection.findOne(query)
+    res.send(result)
+ })
+      app.post('/toys', async (req,res)=> {
+        const newToy = req.body;
+        console.log("post api hitting", newToy);
+        const result = await toyCollection.insertOne(newToy);
+        res.send(result);
+
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
